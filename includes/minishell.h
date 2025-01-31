7fc6aace38eda7d4a68fb7d710f172523b646eef
Filip Balakovski <filip.balakovski@gmail.com>
2025-01-31 13:53:51 +0100

    feat: add initial minishell header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,23 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:15:12 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/01/27 15:15:19 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/01/31 13:52:05 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <sys/wait.h>
+# include <limits.h>
+# include <errno.h>
+# include <signal.h>
+
+#endif
