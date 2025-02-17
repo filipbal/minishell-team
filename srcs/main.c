1f4b4d448258b2831f7276c23bb5e7061a63c130
Ghani Saido <ghanys@centrum.cz>
2025-02-17 20:25:54 +0100

    Created lexer that takes the input string and divide into tokens, modified main.c and minishell.h accordingly. Lexer is working and there are no leaks.

--- srcs/main.c
+++ srcs/main.c
@@ -1,35 +1,44 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ghani <ghani@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:15:53 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/02/07 12:54:24 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/02/17 20:13:13 by ghani            ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	init_shell(t_shell *shell)
+void	init_shell(t_shell *shell)
 {
+	int	i;
+
 	shell->input = NULL;
+	i = 0;
+	while (i < MAX_TOKENS)
+		shell->tokens[i++] = NULL;
 	shell->exit_status = 0;
 	shell->running = 1;
 }
 
-static void	cleanup_shell(t_shell *shell)
+void	cleanup_shell(t_shell *shell)
 {
+	int	i;
+
 	if (shell->input)
 	{
 		free(shell->input);
 		shell->input = NULL;
 	}
-	rl_clear_history();
+	i = 0;
+	while (shell->tokens[i])
+		free(shell->tokens[i++]);
 }
 
-static int	process_input(t_shell *shell)
+int	process_input(t_shell *shell)
 {
 	if (!shell->input)
 	{
@@ -47,18 +56,24 @@ int	main(void)
 {
 	t_shell		shell;
 
-	init_shell(&shell);
+	shell.running = 1;
 	while (shell.running)
 	{
+		init_shell(&shell);
 		shell.input = readline(PROMPT);
 		if (!process_input(&shell))
 		{
 			ft_putstr_fd("\nexit\n", STDERR_FILENO);
 			break ;
 		}
+		tokenize(&shell);
 		free(shell.input);
 		shell.input = NULL;
+		// Next two lines are for testing purposes only
+		for(int i = 0; shell.tokens[i]; i++)
+			printf("%s\n", shell.tokens[i]);
+		cleanup_shell(&shell);
 	}
-	cleanup_shell(&shell);
+	rl_clear_history();
 	return (shell.exit_status);
 }
