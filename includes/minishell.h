b5d17f8427f1c72207420ff19a8a314cf9742408
Filip Balakovski <filip.balakovski@gmail.com>
2025-03-01 10:32:02 +0100

    Implement signal handling for CtrlC, CtrlD, and Ctrl+\

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ghani <ghani@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:15:12 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/02/16 22:26:35 by ghani            ###   ########.fr       */
+/*   Updated: 2025/03/01 09:43:38 by fbalakov         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
@@ -27,6 +27,7 @@
 # include <signal.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <termios.h>
 
 # define PROMPT "minishell$ "
 # define MAX_TOKENS 64
@@ -38,6 +39,11 @@ typedef struct s_shell
 	int		exit_status;
 	int		running;
 }	t_shell;
+
+/* Signal handling */
+extern volatile sig_atomic_t g_signal_received;
+void	signal_handler(int signo);
+void	setup_signals(void);
 
 void	init_shell(t_shell *shell);
 void	cleanup_shell(t_shell *shell);
--- srcs/signals/signals.c
+++ srcs/signals/signals.c
@@ -6,7 +6,56 @@
 /*   By: fbalakov <fbalakov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 15:22:27 by fbalakov          #+#    #+#             */
-/*   Updated: 2025/01/27 15:22:27 by fbalakov         ###   ########.fr       */
+/*   Updated: 2025/03/01 10:05:07 by fbalakov         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
+#include "minishell.h"
+
+/* Global variable to track received signals
+ * Using sig_atomic_t ensures atomic access for signal safety
+ */
+volatile sig_atomic_t	g_signal_received = 0;
+
+/* Signal handler function that processes incoming signals
+ * Updates the global signal state and handles specific signals:
+ * - SIGINT (Ctrl+C): Displays a new prompt on a new line
+ * - SIGQUIT (Ctrl+\): No action needed (handled by SIG_IGN in setup)
+ *
+ * @param signo The signal number received
+ */
+void	signal_handler(int signo)
+{
+	g_signal_received = signo;
+	if (signo == SIGINT)
+	{
+		ft_putstr_fd("\n", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 0);
+		rl_redisplay();
+	}
+}
+
+/* Sets up signal handling for the shell
+ * - Configures terminal settings to hide control character output
+ * - Sets up SIGINT (Ctrl+C) to call our custom handler
+ * - Sets up SIGQUIT (Ctrl+\) to be ignored
+ */
+void	setup_signals(void)
+{
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
+	struct termios		term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	sa_int.sa_handler = signal_handler;
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
+	sa_quit.sa_handler = SIG_IGN;
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_flags = 0;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
